ae30aa4e0493dc900fb45cff75d2bf17ca446547
CCXXXI <ccxxxi@qq.com>
2021-05-31 15:46:05 +0800

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,14 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
+
+    // todo: use okhttp instead of this bullshit
+    implementation("com.github.kittinunf.fuel:fuel:2.3.1")
+    implementation("com.github.kittinunf.fuel:fuel-android:2.3.1")
+    implementation("com.github.kittinunf.fuel:fuel-coroutines:2.3.1")
+
+    implementation("org.jsoup:jsoup:1.13.1")
 }
