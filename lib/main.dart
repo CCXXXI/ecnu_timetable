054dd999b35e2e24685ad891a45cc9e0d4446afd
CCXXXI <ccxxxi@qq.com>
2021-09-23 19:25:44 +0800

    feat: identify users

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 Future<void> main() async {
+  // todo: more user info
+  // todo: clear the currently set user when logout
+  Sentry.configureScope(
+    (scope) => scope.user = SentryUser(
+      id: null,
+      username: null,
+      ipAddress: '{{auto}}',
+    ),
+  );
+
   await SentryFlutter.init(
     (options) {
-      options.dsn =
-          'https://ca1d394e0da94a11a1c32d650b781ea0@o996799.ingest.sentry.io/5975191';
+      options
+        ..dsn =
+            'https://ca1d394e0da94a11a1c32d650b781ea0@o996799.ingest.sentry.io/5975191'
+        ..sendDefaultPii = true;
     },
     appRunner: () => runApp(const MyApp()),
   );
