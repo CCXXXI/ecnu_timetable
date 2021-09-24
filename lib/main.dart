b2b8ef37a9decb6bb001f13fb7b358d1d92d0138
CCXXXI <ccxxxi@qq.com>
2021-09-25 01:10:24 +0800

    fix: import 'package:get/get.dart' hide Trans

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:get/get.dart' hide Trans;
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 Future<void> main() async {
