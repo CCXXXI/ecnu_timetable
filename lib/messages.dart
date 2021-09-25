b7da2efe781d37a8b005193895c5ae04ce606207
CCXXXI <ccxxxi@qq.com>
2021-09-25 13:30:48 +0800

    refactor: rename `Pangu` to `PanGu`

--- lib/messages.dart
+++ lib/messages.dart
@@ -1,14 +1,14 @@
 import 'package:get/get.dart';
 
-var _pangu = _Pangu().spacingText;
+var _panGu = _PanGu().spacingText;
 
 extension PanGu on String {
   String get tr {
-    return _pangu(Trans(this).tr);
+    return _panGu(Trans(this).tr);
   }
 }
 
-class _Pangu {
+class _PanGu {
   final RegExp _cjkQuote = RegExp(
       '([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])(["])');
 
