b7dec306bd9fba219ed64de4aeaffb0e2fee7015
CCXXXI <ccxxxi@qq.com>
2021-09-25 20:53:45 +0800

    fix: use animateToPage only for bottom bar

--- lib/home/home_logic.dart
+++ lib/home/home_logic.dart
@@ -5,18 +5,23 @@ class HomeLogic extends GetxController {
   final idx = 1.obs;
   final pageController = PageController(initialPage: 1);
 
-  void _toPage(int idx_) {
+  void _toPage(int idx_, bool animate) {
     idx.value = idx_;
-    pageController.animateToPage(
-      idx_,
-      duration: const Duration(milliseconds: 500),
-      curve: Curves.ease,
-    );
+
+    if (animate) {
+      pageController.animateToPage(
+        idx_,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.ease,
+      );
+    } else {
+      pageController.jumpToPage(idx_);
+    }
   }
 
-  void onItemSelected(int idx_) => _toPage(idx_);
+  void onItemSelected(int idx_) => _toPage(idx_, true);
 
-  void onDestinationSelected(int idx_) => _toPage(idxMap[idx_]);
+  void onDestinationSelected(int idx_) => _toPage(idxMap[idx_], false);
 }
 
 // NavigationRail: Timetable, Toolbox, Settings
