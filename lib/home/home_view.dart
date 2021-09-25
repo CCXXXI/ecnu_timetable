66d779579886d1a990b699bd864860aa35a8ba8e
CCXXXI <ccxxxi@qq.com>
2021-09-25 16:28:29 +0800

    feat: create home

---
+++ lib/home/home_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'home_logic.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  final logic = Get.put(HomeLogic());
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
