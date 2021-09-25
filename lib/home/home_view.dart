00016e8098ff2f6f26ee65e56bb5d253a59debf9
CCXXXI <ccxxxi@qq.com>
2021-09-25 17:26:24 +0800

    feat: basic BottomNavyBar

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -1,6 +1,8 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../messages.dart';
 import 'home_logic.dart';
 
 class HomePage extends StatelessWidget {
@@ -12,7 +14,35 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Placeholder(),
+      body: PageView(
+        controller: logic.pageController,
+        onPageChanged: logic.onPageChanged,
+        children: const [
+          Placeholder(),
+          Placeholder(),
+          Placeholder(),
+        ],
+      ),
+      bottomNavigationBar: Obx(() {
+        return BottomNavyBar(
+          selectedIndex: logic.idx.value,
+          onItemSelected: logic.onItemSelected,
+          items: [
+            BottomNavyBarItem(
+              title: Text('Timetable'.t),
+              icon: const Icon(Icons.apps),
+            ),
+            BottomNavyBarItem(
+              title: Text('Toolbox'.t),
+              icon: const Icon(Icons.apps),
+            ),
+            BottomNavyBarItem(
+              title: Text('Settings'.t),
+              icon: const Icon(Icons.apps),
+            ),
+          ],
+        );
+      }),
     );
   }
 }
