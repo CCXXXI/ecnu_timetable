1b9485f0c1b446d702c652efb27f1a1c94fbb843
CCXXXI <ccxxxi@qq.com>
2021-09-28 00:05:54 +0800

    fix: record package info manually
    package_info_plus 1.0.6 cannot get real info on windows.

--- lib/utils/messages.dart
+++ lib/utils/messages.dart
@@ -2,6 +2,13 @@ import 'package:get/get.dart';
 
 import 'pangu.dart';
 
+// package_info_plus 1.0.6 cannot get real info on windows
+// record them manually
+const appName = 'ECNU Timetable';
+const packageName = 'io.github.ccxxxi.ecnu_timetable';
+const packageVersion = '0.1.0';
+const buildNumber = '1';
+
 extension Messages on String {
   String get t {
     return spacingText(tr);
