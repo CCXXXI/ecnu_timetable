ecba0136698a5c648e8b69d4a052402f9794ca06
CCXXXI <ccxxxi@qq.com>
2021-10-01 02:04:49 +0800

    refactor: move sentry to utils

--- lib/main.dart
+++ lib/main.dart
@@ -6,31 +6,12 @@ import 'home/home_view.dart';
 import 'utils/desktop.dart';
 import 'utils/logger.dart';
 import 'utils/messages.dart';
+import 'utils/sentry.dart';
 
 Future<void> main() async {
   initLogger();
   if (GetPlatform.isDesktop) initDesktop();
-
-  // todo: more user info
-  // todo: clear the currently set user when logout
-  Sentry.configureScope(
-    (scope) => scope.user = SentryUser(
-      id: null,
-      username: null,
-      ipAddress: '{{auto}}',
-    ),
-  );
-
-  await SentryFlutter.init(
-    (options) {
-      options
-        ..dsn =
-            'https://ca1d394e0da94a11a1c32d650b781ea0@o996799.ingest.sentry.io/5975191'
-        ..sendDefaultPii = true
-        ..release = '$packageName@$version+$buildNumber';
-    },
-    appRunner: () => runApp(const MyApp()),
-  );
+  await initSentry(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
