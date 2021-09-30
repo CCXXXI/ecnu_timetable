8f2d243ab558f97262751260594fa0232bf65e73
CCXXXI <ccxxxi@qq.com>
2021-10-01 01:44:42 +0800

    fix: fix bugs related to animateToPage

--- lib/home/home_logic.dart
+++ lib/home/home_logic.dart
@@ -1,27 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../utils/logger.dart';
+
 class HomeLogic extends GetxController {
   final idx = 1.obs;
   final pageController = PageController(initialPage: 1);
+  final isAnimating = false.obs;
 
-  void _toPage(int idx_, bool animate) {
+  Future<void> _toPage(int idx_, bool animate) async {
     idx.value = idx_;
 
     if (animate) {
-      pageController.animateToPage(
+      logger.d('animateToPage $idx_ begin.');
+      isAnimating.value = true;
+
+      await pageController.animateToPage(
         idx_,
         duration: const Duration(milliseconds: 500),
         curve: Curves.ease,
       );
+
+      isAnimating.value = false;
+      logger.d('animateToPage $idx_ end.');
     } else {
       pageController.jumpToPage(idx_);
     }
   }
 
-  void onItemSelected(int idx_) => _toPage(idx_, true);
-
   void onDestinationSelected(int idx_) => _toPage(idxMap[idx_], false);
+
+  void onItemSelected(int idx_) {
+    if (isAnimating.isFalse) _toPage(idx_, true);
+  }
+
+  void onPageChanged(int idx_) {
+    if (isAnimating.isFalse) idx.value = idx_;
+  }
 }
 
 // NavigationRail: Timetable, Toolbox, Settings
