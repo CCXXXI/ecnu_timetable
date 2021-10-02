b58f3243ae01eae38a1124e99626cc043a5accba
CCXXXI <ccxxxi@qq.com>
2021-10-02 22:22:44 +0800

    refactor: theme

--- lib/settings/theme.dart
+++ lib/settings/theme.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_settings_screens/flutter_settings_screens.dart';
+import 'package:get/get.dart';
 
-ThemeData get lightTheme => ThemeData.light(); // todo
+ThemeData _getTheme(ThemeData base) => base.copyWith(); // todo
 
-ThemeData get darkTheme => ThemeData.dark(); // todo
+ThemeData get lightTheme => _getTheme(ThemeData.light());
+
+ThemeData get darkTheme => _getTheme(ThemeData.dark());
 
 ThemeMode get themeMode => Settings.getValue('themeMode', ThemeMode.system);
+
+ThemeData get theme => themeMode == ThemeMode.dark ||
+        themeMode == ThemeMode.system && Get.isDarkMode
+    ? darkTheme
+    : lightTheme;
