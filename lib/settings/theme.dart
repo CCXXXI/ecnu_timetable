ed12320677a478b23fcc6cd09372e8c4972b2010
CCXXXI <ccxxxi@qq.com>
2021-10-03 02:02:29 +0800

    feat: bug notice

--- lib/settings/theme.dart
+++ lib/settings/theme.dart
@@ -4,15 +4,22 @@ import 'package:get/get.dart';
 
 import '../utils/messages.dart';
 
-ThemeData _getTheme(ThemeData base) => base.copyWith(
-      textTheme: base.textTheme.apply(fontFamily: fontSans),
+ThemeMode get _themeMode =>
+    ThemeMode.values[Settings.getValue('themeMode', ThemeMode.system.index)];
+
+bool get _dark =>
+    _themeMode == ThemeMode.dark ||
+    _themeMode == ThemeMode.system && Get.isPlatformDarkMode;
+
+ThemeData get theme => ThemeData(
+      brightness: _dark ? Brightness.dark : Brightness.light,
+      fontFamily: fontSans,
     );
 
-ThemeMode get _themeMode => Settings.getValue('themeMode', ThemeMode.system);
-
-ThemeData get theme => _getTheme(
-      _themeMode == ThemeMode.dark ||
-              _themeMode == ThemeMode.system && Get.isPlatformDarkMode
-          ? ThemeData.dark()
-          : ThemeData.light(),
+// void updateTheme() => Get.changeTheme(theme);
+// todo: wait for https://github.com/jonataslaw/getx/issues/1878
+void updateTheme() => Get.snackbar(
+      '主题切换目前有 bug，请手动重启以使更改生效',
+      'https://github.com/jonataslaw/getx/issues/1878',
+      duration: const Duration(seconds: 10),
     );
