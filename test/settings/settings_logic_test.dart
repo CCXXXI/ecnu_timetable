4dabc4d33ec72b7ef14ee911db0746dd520caf4e
CCXXXI <ccxxxi@qq.com>
2021-10-04 03:15:57 +0800

    test(settings): add more tests for settings_logic

--- test/settings/settings_logic_test.dart
+++ test/settings/settings_logic_test.dart
@@ -1,11 +1,17 @@
 import 'package:ecnu_timetable/settings/settings_logic.dart';
+import 'package:ecnu_timetable/utils/messages.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
 
 class MockSettingsLogic extends SettingsLogic {
   @override
   final String? id, username;
+  final String latestVer_;
 
-  MockSettingsLogic({this.id, this.username});
+  MockSettingsLogic({this.id, this.username, this.latestVer_ = version});
+
+  @override
+  void updateVerInfo() async => latestVer.value = latestVer_;
 }
 
 const mockId = '10101001000';
@@ -13,18 +19,33 @@ const mockUsername = 'ECNUer';
 
 void main() {
   group('Anonymous', () {
-    final logic = MockSettingsLogic();
+    final logic = Get.put(MockSettingsLogic());
 
     test('not logged in', () => expect(logic.loggedIn, isFalse));
     test('id is null', () => expect(logic.id, isNull));
     test('username is null', () => expect(logic.username, isNull));
+
+    Get.delete<MockSettingsLogic>();
   });
 
   group('User', () {
-    final logic = MockSettingsLogic(id: mockId, username: mockUsername);
+    final logic =
+        Get.put(MockSettingsLogic(id: mockId, username: mockUsername));
 
     test('logged in', () => expect(logic.loggedIn, isTrue));
     test('id', () => expect(logic.id, mockId));
     test('username', () => expect(logic.username, mockUsername));
+
+    Get.delete<MockSettingsLogic>();
+  });
+
+  group('Update', () {
+    final logic0 = Get.put(MockSettingsLogic());
+    test('not available', () => expect(logic0.updateAvailable, isFalse));
+    Get.delete<MockSettingsLogic>();
+
+    final logic1 = Get.put(MockSettingsLogic(latestVer_: '1024.2048.4096'));
+    test('available', () => expect(logic1.updateAvailable, isTrue));
+    Get.delete<MockSettingsLogic>();
   });
 }
