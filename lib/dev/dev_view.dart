3a10e421b6383d04491b5adef3f58af34262dad1
CCXXXI <ccxxxi@qq.com>
2021-10-05 03:22:17 +0800

    feat(toolbox): secret tools

--- lib/dev/dev_view.dart
+++ lib/dev/dev_view.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 import 'package:get/get.dart';
 
+import '../toolbox/toolbox_logic.dart';
 import 'dev_logic.dart';
 
 class DevPage extends StatelessWidget {
   DevPage({Key? key}) : super(key: key);
 
   final logic = Get.put(DevLogic());
+  final toolboxLogic = Get.find<ToolboxLogic>();
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +17,25 @@ class DevPage extends StatelessWidget {
       appBar: AppBar(title: const Text('开发者选项')),
       body: ListView(
         children: [
-          ListTile(
-            title: const Text('Trivia'),
-            onTap: logic.triviaOnTap,
-          )
+          SettingsGroup(
+            title: '工具箱',
+            children: [
+              SwitchSettingsTile(
+                title: 'sucker',
+                settingKey: 'toolbox.sucker',
+                onChange: (v) => toolboxLogic.updateSucker(v),
+              ),
+            ],
+          ),
+          SettingsGroup(
+            title: '杂项',
+            children: [
+              ListTile(
+                title: const Text('Trivia'),
+                onTap: logic.triviaOnTap,
+              ),
+            ],
+          ),
         ],
       ),
     );
--- lib/toolbox/toolbox_logic.dart
+++ lib/toolbox/toolbox_logic.dart
@@ -1,6 +1,17 @@
+import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 import 'package:get/get.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+import '../utils/gu.dart';
+
 class ToolboxLogic extends GetxController {
   void Function() l(String url) => () => launch(url);
+
+  final sucker = Settings.getValue('toolbox.sucker', false).obs;
+
+  updateSucker(bool v) => sucker.value = v;
+
+  void suckerOnTap() {
+    gu();
+  }
 }
