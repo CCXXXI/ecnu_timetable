186bbe710252c6482885bdf228ed5047be769966
CCXXXI <ccxxxi@qq.com>
2021-10-05 02:19:27 +0800

    refactor(toolbox): remove unnecessary as

--- lib/toolbox/toolbox_logic.dart
+++ lib/toolbox/toolbox_logic.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
-import 'package:url_launcher/url_launcher.dart' as launcher;
+import 'package:url_launcher/url_launcher.dart';
 
 class ToolboxLogic extends GetxController {
-  void Function() l(String url) => () => launcher.launch(url);
+  void Function() l(String url) => () => launch(url);
 }
