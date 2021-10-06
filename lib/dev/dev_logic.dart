2c0186a6193a6e9c4edd49cd6045a244b97479d7
CCXXXI <ccxxxi@qq.com>
2021-10-07 04:38:14 +0800

    feat: notice when change log settings

--- lib/dev/dev_logic.dart
+++ lib/dev/dev_logic.dart
@@ -4,4 +4,6 @@ import 'trivia_view.dart';
 
 class DevLogic extends GetxController {
   void triviaOnTap() => Get.to(() => const TriviaPage());
+
+  void updateLog(_) => Get.snackbar('日志设置已更新', '重启后生效');
 }
