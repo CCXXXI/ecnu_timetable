1c3f4fea7291757188094561021c52b781a245db
CCXXXI <ccxxxi@qq.com>
2021-10-07 05:18:06 +0800

    feat(utils): LoggyDioInterceptor

--- lib/utils/dio.dart
+++ lib/utils/dio.dart
@@ -1,8 +1,11 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_loggy_dio/flutter_loggy_dio.dart';
 
 final dio = Dio()
-  ..interceptors.add(LogInterceptor(
+  ..interceptors.add(LoggyDioInterceptor(
+    requestHeader: true,
     requestBody: true,
+    responseHeader: true,
     responseBody: true,
-    // logPrint: logger.v,
+    error: true,
   ));
