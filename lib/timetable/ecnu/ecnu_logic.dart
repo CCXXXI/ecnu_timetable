e64d659ada79a48962eaa6a9f9aae90093c6be3e
CCXXXI <ccxxxi@qq.com>
2021-10-10 20:21:29 +0800

    feat(timetable): more desc

--- lib/timetable/ecnu/ecnu_logic.dart
+++ lib/timetable/ecnu/ecnu_logic.dart
@@ -1,3 +1,19 @@
 import 'package:get/get.dart';
 
-class EcnuLogic extends GetxController {}
+import '../../utils/log.dart';
+
+class EcnuLogic extends GetxController with L {
+  /// - 0: login
+  /// - 1: check
+  final step = 0.obs;
+
+  void onStepContinue() {
+    l.debug('step: ${step.value}');
+    if (step.value == 0) {
+      // todo
+      step.value++;
+    } else {
+      Get.back();
+    }
+  }
+}
--- lib/timetable/ecnu/ecnu_view.dart
+++ lib/timetable/ecnu/ecnu_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../utils/messages.dart';
 import 'ecnu_logic.dart';
 
 class EcnuPage extends StatelessWidget {
@@ -14,22 +15,43 @@ class EcnuPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('自公共数据库导入课表'),
       ),
-      body: Stepper(
-        steps: const [
-          Step(
-            title: Text('登录公共数据库'),
-            content: Placeholder(
-              color: Colors.red,
+      body: Obx(
+        () => Stepper(
+          currentStep: logic.step.value,
+          onStepContinue: logic.onStepContinue,
+          controlsBuilder: controlsBuilder,
+          steps: [
+            Step(
+              title: const Text('登录公共数据库'),
+              subtitle: Text('密码仅用于登录，可至GitHub检查源码。'.s),
+              content: const Placeholder(
+                // todo
+                color: Colors.red,
+              ),
+              state: logic.step.value == 0
+                  ? StepState.indexed
+                  : StepState.disabled,
             ),
-          ),
-          Step(
-            title: Text('确认课表内容'),
-            content: Placeholder(
-              color: Colors.green,
+            Step(
+              title: const Text('确认课表内容'),
+              subtitle: Text('有误可至GitHub反馈。'.s),
+              content: const Placeholder(
+                // todo
+                color: Colors.green,
+              ),
+              state: logic.step.value == 1
+                  ? StepState.indexed
+                  : StepState.disabled,
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
+
+  Widget controlsBuilder(BuildContext context, ControlsDetails details) =>
+      ElevatedButton(
+        onPressed: details.onStepContinue,
+        child: Text(['登录', '完成'][details.stepIndex]),
+      );
 }
