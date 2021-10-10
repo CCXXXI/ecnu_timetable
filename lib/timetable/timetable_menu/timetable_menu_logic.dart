4fb618235129f6ff69f88c7eb09a7137d32b7bad
CCXXXI <ccxxxi@qq.com>
2021-10-11 03:35:47 +0800

    fix(timetable): disable idc on web

--- lib/timetable/timetable_menu/timetable_menu_logic.dart
+++ lib/timetable/timetable_menu/timetable_menu_logic.dart
@@ -1,10 +1,25 @@
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+import '../../settings/settings_logic.dart';
 import '../../utils/gu.dart';
+import '../../utils/messages.dart';
 import '../ecnu/ecnu_view.dart';
 
 class TimetableMenuLogic extends GetxController {
-  void ecnuOnTap() => Get.to(() => EcnuPage());
+  void ecnuOnTap() {
+    if (GetPlatform.isWeb) {
+      Get.defaultDialog(
+        title: 'Web端功能受限'.s,
+        middleText: '因跨域资源共享（CORS）问题，无法连接公共数据库。',
+        textConfirm: '下载完整版',
+        textCancel: '返回',
+        onConfirm: () => launch(SettingsLogic.latestUrl),
+      );
+    } else {
+      Get.to(() => EcnuPage());
+    }
+  }
 
   void htmlOnTap() => gu();
 }
