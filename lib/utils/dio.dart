a33c869947c764f0a19db521fa3fb5737c470d41
CCXXXI <ccxxxi@qq.com>
2021-10-11 01:41:30 +0800

    feat(utils): add CookieManager to Dio

--- lib/utils/dio.dart
+++ lib/utils/dio.dart
@@ -1,14 +1,19 @@
+import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio/dio.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:flutter_loggy_dio/flutter_loggy_dio.dart';
 import 'package:loggy/loggy.dart';
 
 final dio = Dio()
-  ..interceptors.add(LoggyDioInterceptor(
-    requestHeader: true,
-    requestBody: true,
-    responseHeader: true,
-    responseBody: true,
-    error: true,
-    requestLevel: LogLevel.debug,
-    responseLevel: LogLevel.debug,
-  ));
+  ..interceptors.addAll([
+    LoggyDioInterceptor(
+      requestHeader: true,
+      requestBody: true,
+      responseHeader: true,
+      responseBody: true,
+      error: true,
+      requestLevel: LogLevel.debug,
+      responseLevel: LogLevel.debug,
+    ),
+    CookieManager(CookieJar()),
+  ]);
