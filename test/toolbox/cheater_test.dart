be91a90c5f1a4d630a74ca30f91b32f853658d69
CCXXXI <ccxxxi@qq.com>
2021-10-16 04:14:10 +0800

    test(toolbox): add test for cheater

---
+++ test/toolbox/cheater_test.dart
@@ -0,0 +1,6 @@
+import 'package:ecnu_timetable/toolbox/cheater.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('cheater', () => expect(randomCheater, isNotEmpty));
+}
