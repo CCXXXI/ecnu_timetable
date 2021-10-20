475fd4a496f63cea97af70fbbb44a2e313074c6f
CCXXXI <ccxxxi@qq.com>
2021-10-20 15:00:22 +0800

    chore: 0.13.0+17

--- lib/utils/string.dart
+++ lib/utils/string.dart
@@ -7,8 +7,8 @@ import 'pangu.dart';
 // record them manually
 const appName = 'ECNU Timetable';
 const packageName = 'io.github.ccxxxi.ecnu_timetable';
-const version = '0.12.0';
-const buildNumber = '16';
+const version = '0.13.0';
+const buildNumber = '17';
 
 const release = '$packageName@$version+$buildNumber';
 
