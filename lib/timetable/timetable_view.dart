a3aff6b828b4c36d23e95a331fb2507c963785b3
CCXXXI <ccxxxi@qq.com>
2021-10-23 16:24:58 +0800

    feat(timetable): basic timetable view

--- lib/timetable/timetable_view.dart
+++ lib/timetable/timetable_view.dart
@@ -22,7 +22,53 @@ class TimetablePage extends StatelessWidget {
             child: Text('NO DATA'),
           );
         } else {
-          return LayoutGrid(columnSizes: const [auto], rowSizes: const [auto]);
+          return LayoutGrid(
+            areas: TimetableLogic.areas,
+            columnSizes: [auto, ...TimetableLogic.weekdays.map((i) => 1.fr)],
+            rowSizes: [auto, ...TimetableLogic.units.map((i) => 1.fr)],
+            children: [
+              ...TimetableLogic.weekdays.map(
+                (i) => Center(
+                  child: Text(
+                    ['一', '二', '三', '四', '五', '六', '日'][i],
+                    textAlign: TextAlign.center,
+                  ),
+                ).inGridArea('$i-x'),
+              ),
+              ...TimetableLogic.units.map(
+                (i) => Center(
+                  child: Text(
+                    [
+                      '8:00\n8:45',
+                      '8:50\n9:35',
+                      '9:50\n10:35',
+                      '10:40\n11:25',
+                      '11:30\n12:15',
+                      '13:00\n13:45',
+                      '13:50\n14:35',
+                      '14:50\n15:35',
+                      '15:40\n16:25',
+                      '16:30\n17:15',
+                      '18:00\n18:45',
+                      '18:50\n19:35',
+                      '19:40\n20:25',
+                    ][i],
+                    textAlign: TextAlign.center,
+                  ),
+                ).inGridArea('x-$i'),
+              ),
+              for (final course in courses.values)
+                for (final period in course.periods!)
+                  Center(
+                    child: Text(
+                      '${course.courseName}',
+                      textAlign: TextAlign.center,
+                    ),
+                  ).inGridArea(
+                    '${period.weekday}-${period.unit}',
+                  ),
+            ],
+          );
         }
       },
     );
