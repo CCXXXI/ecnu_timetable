0e3019fac156856f6d126e285b9b93443f342373
CCXXXI <ccxxxi@qq.com>
2021-10-24 13:18:34 +0800

    test(utils): test Loading

---
+++ test/utils/loading_test.dart
@@ -0,0 +1,11 @@
+import 'package:ecnu_timetable/utils/loading.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('loading', (tester) async {
+    await tester.pumpWidget(MaterialApp(home: Scaffold(body: Loading())));
+
+    expect(find.byType(IconButton), findsOneWidget);
+  });
+}
