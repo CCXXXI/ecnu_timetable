16ffdf8f70386d8907255dd554086b39e6adc974
CCXXXI <ccxxxi@qq.com>
2021-10-24 16:46:26 +0800

    feat(toolbox): reorderable toolbox
    resolve #123

--- lib/toolbox/toolbox_menu/toolbox_menu_view.dart
+++ lib/toolbox/toolbox_menu/toolbox_menu_view.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:quiver/iterables.dart';
 
+import '../../utils/database.dart';
+import '../toolbox_view.dart';
 import 'toolbox_menu_logic.dart';
 
 class ToolboxMenuPage extends StatelessWidget {
@@ -10,6 +15,8 @@ class ToolboxMenuPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final tools_ = tools();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('工具箱'),
@@ -17,11 +24,37 @@ class ToolboxMenuPage extends StatelessWidget {
       body: Center(
         child: ConstrainedBox(
           constraints: const BoxConstraints(maxWidth: 600),
-          child: ListView(
-            children: const [Placeholder()],
+          child: ValueListenableBuilder(
+            valueListenable: conf.listenable(keys: ['toolbox']),
+            builder: (_, __, ___) => ReorderableListView(
+              children: range(tools_.length)
+                  .cast<int>()
+                  .map((i) => tools_[toolbox.order[i]].toTile())
+                  .toList(),
+              onReorder: toolbox.reorder,
+            ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class ToolTile extends StatelessWidget {
+  const ToolTile(
+    this.leading,
+    this.title, {
+    required Key key,
+  }) : super(key: key);
+
+  final IconData leading;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: FaIcon(leading),
+      title: Text(title),
     );
   }
 }
--- lib/toolbox/toolbox_view.dart
+++ lib/toolbox/toolbox_view.dart
@@ -3,11 +3,15 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:quiver/iterables.dart';
 
+import '../utils/database.dart';
 import '../utils/loading.dart';
 import '../utils/string.dart';
 import '../utils/web.dart';
 import 'toolbox_logic.dart';
+import 'toolbox_menu/toolbox_menu_view.dart';
 
 class ToolboxPage extends StatelessWidget {
   ToolboxPage({Key? key}) : super(key: key);
@@ -16,80 +20,104 @@ class ToolboxPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() {
-      return GridView.extent(
+    final tools_ = tools(logic);
+
+    return ValueListenableBuilder(
+      valueListenable: conf.listenable(keys: ['toolbox']),
+      builder: (_, __, ___) => GridView.extent(
         maxCrossAxisExtent: 512,
         childAspectRatio: pi,
         crossAxisSpacing: 16,
         mainAxisSpacing: 16,
-        children: tools.map((e) => e.toCard()).toList(),
-      );
-    });
+        children: range(tools_.length)
+            .cast<int>()
+            .map((i) => tools_[toolbox.order[i]].toCard())
+            .toList(),
+      ),
+    );
   }
-
-  List<Tool> get tools => [
-        Tool(
-          FontAwesomeIcons.dog,
-          '/sucker',
-          logic.sucker.value.length < 16
-              ? logic.sucker.value
-              : logic.sucker.value.substring(0, 16) + '……',
-          onTap: logic.suckerOnTap,
-          enabled: !GetPlatform.isWeb,
-        ),
-        Tool(
-          FontAwesomeIcons.cat,
-          '/cheater',
-          logic.cheater.value.length < 16
-              ? logic.cheater.value
-              : logic.cheater.value.substring(0, 16) + '……',
-          onTap: logic.cheaterOnTap,
-        ),
-        Tool(
-          FontAwesomeIcons.graduationCap,
-          '卷课意愿值估算',
-          '仅供参考',
-          onTap: logic.juanOnTap,
-        ),
-        Tool(
-          FontAwesomeIcons.calendarAlt,
-          '校历',
-          '长按打开网页版',
-          onTap: logic.calendarOnTap,
-          onLongPress: Url.calendar.launch,
-        ),
-        Tool(
-          FontAwesomeIcons.scroll,
-          '公告',
-          '善用搜索',
-          onTap: Url.announcements.launch,
-        ),
-        Tool(
-          FontAwesomeIcons.mapMarked,
-          '校内地图',
-          '2D/3D',
-          onTap: Url.map.launch,
-        ),
-        Tool(
-          FontAwesomeIcons.bus,
-          '校车时刻表',
-          '需要连学校Wifi/VPN'.s,
-          onTap: Url.bus.launch,
-        ),
-        Tool(
-          FontAwesomeIcons.cube,
-          'ECNU软件镜像站'.s,
-          '内容很少',
-          onTap: Url.mirrors.launch,
-        ),
-        Tool(
-          FontAwesomeIcons.key,
-          '学校VPN'.s,
-          '对校外网站有减速作用',
-          onTap: Url.vpn.launch,
-        ),
-      ];
 }
+
+List<Tool> tools([ToolboxLogic? logic]) => [
+      Tool(
+        FontAwesomeIcons.dog,
+        '/sucker',
+        '',
+        subtitleWidget: logic == null
+            ? null
+            : Obx(
+                () => logic.sucker.isEmpty
+                    ? Loading()
+                    : Text(
+                        logic.sucker.value.length < 16
+                            ? logic.sucker.value
+                            : logic.sucker.value.substring(0, 16) + '……',
+                      ),
+              ),
+        onTap: logic?.suckerOnTap,
+        enabled: !GetPlatform.isWeb,
+      ),
+      Tool(
+        FontAwesomeIcons.cat,
+        '/cheater',
+        '',
+        subtitleWidget: logic == null
+            ? null
+            : Obx(
+                () => logic.cheater.isEmpty
+                    ? Loading()
+                    : Text(
+                        logic.cheater.value.length < 16
+                            ? logic.cheater.value
+                            : logic.cheater.value.substring(0, 16) + '……',
+                      ),
+              ),
+        onTap: logic?.cheaterOnTap,
+      ),
+      Tool(
+        FontAwesomeIcons.graduationCap,
+        '卷课意愿值估算',
+        '仅供参考',
+        onTap: logic?.juanOnTap,
+      ),
+      Tool(
+        FontAwesomeIcons.calendarAlt,
+        '校历',
+        '长按打开网页版',
+        onTap: logic?.calendarOnTap,
+        onLongPress: Url.calendar.launch,
+      ),
+      Tool(
+        FontAwesomeIcons.scroll,
+        '公告',
+        '善用搜索',
+        onTap: Url.announcements.launch,
+      ),
+      Tool(
+        FontAwesomeIcons.mapMarked,
+        '校内地图',
+        '2D/3D',
+        onTap: Url.map.launch,
+      ),
+      Tool(
+        FontAwesomeIcons.bus,
+        '校车时刻表',
+        '需要连学校Wifi/VPN'.s,
+        onTap: Url.bus.launch,
+      ),
+      Tool(
+        FontAwesomeIcons.cube,
+        'ECNU软件镜像站'.s,
+        '内容很少',
+        onTap: Url.mirrors.launch,
+      ),
+      Tool(
+        FontAwesomeIcons.key,
+        '学校VPN'.s,
+        '对校外网站有减速作用',
+        onTap: Url.vpn.launch,
+      ),
+    ];
 
 class Tool {
   const Tool(
@@ -99,11 +127,13 @@ class Tool {
     this.onTap,
     this.onLongPress,
     this.enabled = true,
+    this.subtitleWidget,
   });
 
   final IconData leading;
   final String title;
   final String subtitle;
+  final Widget? subtitleWidget;
   final void Function()? onTap;
   final void Function()? onLongPress;
   final bool enabled;
@@ -115,6 +145,13 @@ class Tool {
         onTap: onTap,
         onLongPress: onLongPress,
         enabled: enabled,
+        subtitleWidget: subtitleWidget,
+      );
+
+  ToolTile toTile() => ToolTile(
+        leading,
+        title,
+        key: Key(title),
       );
 }
 
@@ -127,11 +164,13 @@ class ToolCard extends StatelessWidget {
     this.onTap,
     this.onLongPress,
     this.enabled = true,
+    this.subtitleWidget,
   }) : super(key: key);
 
   final IconData leading;
   final String title;
   final String subtitle;
+  final Widget? subtitleWidget;
   final void Function()? onTap;
   final void Function()? onLongPress;
   final bool enabled;
@@ -152,7 +191,7 @@ class ToolCard extends StatelessWidget {
           child: ListTile(
             leading: FaIcon(leading),
             title: Text(title),
-            subtitle: subtitle.isEmpty ? Loading() : Text(subtitle),
+            subtitle: subtitleWidget ?? Text(subtitle),
             mouseCursor: MouseCursor.uncontrolled,
           ),
         ),
